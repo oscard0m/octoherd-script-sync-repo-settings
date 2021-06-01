117356c87eef07396e91214e9ee4017cf01cbceb
Oscar Dominguez <dominguez.celada@gmail.com>
2021-06-01 11:15:58 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * This script takes settings in a .github/sync-repo-settings.yaml file, and automatically applies them to your repository
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
